23a18fa2d73a635b6978d3f3ee025c1e9b7fea28
juancamilosalazarrestrepo <juancamilosalazarrestrepo@gmail.com>
2023-03-13 01:02:11 -0500

    add not Found page

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,5 @@
+function NotFoundPage () {
+  return <div>Pagina No Encontrada</div>
+}
+
+export default NotFoundPage
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import TablePage from '../pages/TablePage/TablePage'
 import CreatePost from '../pages/CreatePosts/CreatePost'
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage'
 
 export const routes = (
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<TablePage />} exact />
       <Route path='/create' element={<CreatePost />} exact />
+      <Route path='/*' element={<NotFoundPage />} exact />
     </Routes>
   </BrowserRouter>
 )
